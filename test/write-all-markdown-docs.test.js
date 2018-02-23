07f13559557a2f617a81f96c6bb729067cac235e
Kevin Delisle <kjdelisle@gmail.com>
2018-02-23 10:50:40 -0500

    refactor: create mkTmpDir utility function
    DRY; move the logic to create temporary directories to a utility
    since we'll also need this logic to store repo copies.

--- test/write-all-markdown-docs.test.js
+++ test/write-all-markdown-docs.test.js
@@ -1,23 +1,16 @@
 'use strict';
 
 const _ = require('lodash');
-const fs = require('fs');
-const os = require('os');
 const path = require('path');
-const promisify = require('util').promisify;
 const tap = require('tap');
 
-const crypto = require('crypto');
-const mkdirAsync = promisify(fs.mkdir);
-
+const mkTmpDir = require('../lib/utils').mkTmpDir;
 const getAllMarkdownDocs = require('../lib/get-all-markdown-docs');
 const writeAllMarkdownDocs = require('../lib/write-all-markdown-docs');
 
 tap.test('write-all-markdown-docs', async t => {
-  const tmpDir = path.join(os.tmpdir(), crypto.randomBytes(32).toString('hex'));
-  await mkdirAsync(tmpDir);
   const docPath = path.resolve(__dirname, 'fixtures', 'fake-docs');
-
+  const tmpDir = await mkTmpDir();
   const docs = await getAllMarkdownDocs(docPath);
   await writeAllMarkdownDocs(tmpDir, docs);
   const newDocs = await getAllMarkdownDocs(tmpDir);
